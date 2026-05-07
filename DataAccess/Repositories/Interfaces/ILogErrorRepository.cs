e6aae7c7046799bdfda6d5b5aaf062eefc149668
Jais Hedegaard Andersen <HKF44JAA@hk.dk>
2026-05-07 12:41:09 +0200

    Interface and class for repo created.

---
+++ DataAccess/Repositories/Interfaces/ILogErrorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories.Interfaces
+{
+    public interface ILogErrorRepository
+    {
+        Task AddAsync(LogError logError);
+    }
+}
---
+++ DataAccess/Repositories/LogErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    internal class LogErrorRepository : ILogErrorRepository
+    {
+        public Task AddAsync(LogError logError)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
