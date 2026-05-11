d951513af1093c28ed9ab80f0b9daa1f93a86b4e
Jais Hedegaard Andersen <jaa007@edu.zealand.dk>
2026-05-11 14:30:45 +0200

    Service interfaces added.

---
+++ LogWatcher/Services/Interfaces/ILogFileReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogWatcher.Services.Interfaces
+{
+    public interface ILogFileReader
+    {
+        Task<(IReadOnlyList<string> Lines, long NewOffset)> ReadNewLinesAsync(string filePath, long fromOffset, CancellationToken cancellationToken = default);
+    }
+}
---
+++ LogWatcher/Services/Interfaces/ILogParser.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogWatcher.Services.Interfaces
+{
+    public interface ILogParser
+    {
+        IReadOnlyList<LogError> Parse(IReadOnlyList<string> lines, Guid logFileId, string filePath);
+    }
+}
