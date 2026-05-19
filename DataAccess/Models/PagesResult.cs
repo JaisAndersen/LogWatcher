63a42d4afec1c8a2e981c1cd9d0e8c661daf3a12
Jais Hedegaard Andersen <jais.h.sorensen@gmail.com>
2026-05-19 10:18:40 +0200

    PagesResult added and files moved.

---
+++ DataAccess/Models/PagesResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class PagesResult<T>
+    {
+        public List<T> Items { get; set; } new();
+        public int TotalCount { get; set; }
+        public int Paged { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
--- DataAccess/Repositories/Interfaces/ILogErrorRepository.cs
+++ DataAccess/Repositories/Interfaces/ILogErrorRepository.cs
@@ -5,5 +5,7 @@ namespace DataAccess.Repositories.Interfaces
     public interface ILogErrorRepository
     {
         Task AddRangeAsync(IEnumerable<LogError> errors);
+        Task<PagedResult<LogError>> GetAsync(LogErrorQuery query);
+        Task AcknowledgeAsync(Guid id);
     }
 }
