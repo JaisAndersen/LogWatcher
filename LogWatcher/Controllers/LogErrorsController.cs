ca49e4c7493a784fc6096eb093c11baf427a146d
Jais Hedegaard Andersen <jais.h.sorensen@gmail.com>
2026-05-19 13:43:59 +0200

    AspNetCore added to project and controller class created.

---
+++ LogWatcher/Controllers/LogErrorsController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogWatcher.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogErrorsController : ControllerBase
+    {
+        private readonly ILogErrorRepository _repository;
+
+        public LogErrorsController(ILogErrorRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<LogError>>> Get([FromQuery] LogErrorQuery query)
+        {
+            if (query.From.HasValue && query.To.HasValue && query.From > query.To)
+                return BadRequest("From has to be before To");
+
+            var result = await _repository.GetAsync(query);
+            return Ok(result);
+        }
+
+        [HttpPatch("{id}/acknowledge")]
+        public async Task<IActionResult> Acknowledge(Guid id)
+        {
+            await _repository.AcknowledgeAsync(id);
+            return NoContent();
+        }
+    }
+}
