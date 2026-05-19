6581f2bfa8f0afd0b8aaab0d095b9d2c862cbf17
Jais Hedegaard Andersen <jais.h.sorensen@gmail.com>
2026-05-19 09:12:37 +0200

    Model for filtering added.

---
+++ LogWatcher/Models/LogErrorQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogWatcher.Models
+{
+    public class LogErrorQuery
+    {
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 50;
+    }
+}
